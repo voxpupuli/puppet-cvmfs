acc3d505acb5e8736ba3d1e02b71d05769a90ba6
Steve Traylen <steve.traylen@cern.ch>
2013-09-17 14:23:25 +0200

    Put yumrepos in seperate file for use by server installation.

--- manifests/install.pp
+++ manifests/install.pp
@@ -19,12 +19,11 @@
 #
 class cvmfs::install (
     $cvmfs_version = $cvmfs::params::cvmfs_version,
-    $cvmfs_yum = $cvmfs::params::cvmfs_yum,
-    $cvmfs_yum_testing = $cvmfs::params::cvmfs_yum_testing,
-    $cvmfs_yum_testing_enabled = $cvmfs::params::cvmfs_yum_testing_enabled,
     $cvmfs_cache_base = $cvmfs::params::cvmfs_cache_base,
     $default_cvmfs_cache_base = $cvmfs::params::default_cvmfs_cache_base,
 ) inherits cvmfs::params {
+
+   class{'cvmfs::yum':}
 
    # Create the cache dir if one is defined, otherwise assume default is in the package.
    # Require the package so we know the user is in place.
@@ -53,36 +52,6 @@ class cvmfs::install (
    }
 
    $major = $cvmfs::params::major_release
-   yumrepo{'cvmfs':
-      descr       => "CVMFS yum repository for el${major}",
-      baseurl     => "$cvmfs_yum",
-      gpgcheck    => 1,
-      gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM',
-      enabled     => 1,
-      includepkgs => 'cvmfs,cvmfs-keys',
-      priority    => 80,
-      require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM']
-   }
-   yumrepo{'cvmfs-testing':
-      descr       => "CVMFS yum testing repository for el${major}",
-      baseurl     => "$cvmfs_yum_testing",
-      gpgcheck    => 1,
-      gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM',
-      enabled     => $cvmfs_yum_testing_enabled,
-      includepkgs => 'cvmfs,cvmfs-keys',
-      priority    => 80,
-      require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM']
-   }
-
-   # Copy out the gpg key once only ever.
-   file{'/etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM':
-      ensure  => file,
-      source  => 'puppet:///modules/cvmfs/RPM-GPG-KEY-CernVM',
-      replace => false,
-      owner   => root,
-      group   => root,
-      mode    => '0644'
-   }
 
    # Create a file for the cvmfs
    file{'/etc/cvmfs/cvmfsfacts.yaml':
