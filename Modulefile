a76ee0d0fd91847c4ab99cfdfcb9c8e0694a9aed
Steve Traylen <steve.traylen@cern.ch>
2014-06-18 16:17:13 +0200

    Seperate yum repo conf for clients and servers

--- Modulefile
+++ Modulefile
@@ -1,5 +1,5 @@
 name    'CERNOps-cvmfs'
-version '0.2.2'
+version '0.3.1'
 source 'https://github.com/cvmfs/puppet-cvmfs'
 author 'CERNOps'
 license 'Apache License, Version 2.0'
--- manifests/server/yum.pp
+++ manifests/server/yum.pp
@@ -3,9 +3,41 @@ class cvmfs::server::yum (
   $cvmfs_yum_kernel         = $cvmfs::params::cvmfs_yum_kernel,
   $cvmfs_yum_kernel_enabled = $cvmfs::params::cvmfs_yum_kernel_enabled,
   $major_release            = $cmvfs::params::major_release
-) inherits cvmfs::params {
+) inherits cvmfs::server {
 
-  class{'cvmfs::yum':}
+  $major = $cvmfs::params::major_release
+  yumrepo{'cvmfs':
+    descr       => "CVMFS yum repository for el${major}",
+    baseurl     => $cvmfs_yum,
+    gpgcheck    => 1,
+    gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM',
+    enabled     => 1,
+    includepkgs => 'cvmfs,cvmfs-keys,cvmfs-server',
+    priority    => 80,
+    require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM']
+  }
+  yumrepo{'cvmfs-testing':
+    descr       => "CVMFS yum testing repository for el${major}",
+    baseurl     => $cvmfs_yum_testing,
+    gpgcheck    => 1,
+    gpgkey      => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM',
+    enabled     => $cvmfs_yum_testing_enabled,
+    includepkgs => 'cvmfs,cvmfs-keys,cvmfs-server',
+    priority    => 80,
+    require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM']
+  }
+
+  # Copy out the gpg key once only ever.
+  file{'/etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM':
+    ensure  => file,
+    source  => 'puppet:///modules/cvmfs/RPM-GPG-KEY-CernVM',
+    replace => false,
+    owner   => root,
+    group   => root,
+    mode    => '0644'
+  }
+
+
 
   yumrepo{'cvmfs-kernel':
     descr       => "CVMFS yum kernel repository for el${major_release}",
