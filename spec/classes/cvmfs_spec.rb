c5aabac822f9ef45e105bddf3b545c37407b03ca
Steve Traylen <steve.traylen@cern.ch>
2015-04-29 20:32:14 +0200

    Add rspec tests for autofs configuration

--- spec/classes/cvmfs_spec.rb
+++ spec/classes/cvmfs_spec.rb
@@ -19,6 +19,28 @@ describe 'cvmfs' do
                     :cvmfsversion => '2.1.20', :cvmfspartsize => '20000'}}
       it { should contain_class('cvmfs::config') }
       it { should contain_class('cvmfs::service') }
+      it { should contain_service('autofs') }
+
+
+      context 'with manage_autofs_service true' do
+        let(:params) {{:manage_autofs_service => true}}
+        it { should contain_service('autofs') }
+      end
+      context 'with manage_autofs_service false' do
+        let(:params) {{:manage_autofs_service => false}}
+        it { should_not contain_service('autofs') }
+      end
+
+      it { should contain_augeas('cvmfs_automaster') }
+      context 'with config_automaster true' do
+        let(:params) {{:config_automaster  => true}}
+        it { should contain_augeas('cvmfs_automaster') }
+      end
+      context 'with config_automaster false' do
+        let(:params) {{:config_automaster  => false}}
+        it { should_not contain_augeas('cvmfs_automaster') }
+      end
+
     end
 
   end
