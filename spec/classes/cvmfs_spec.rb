4010bf7ae60ba935043c104bb5314f0234692cf2
Steve Traylen <steve.traylen@cern.ch>
2015-05-06 12:18:37 +0200

    Add tests for yumrepos

--- spec/classes/cvmfs_spec.rb
+++ spec/classes/cvmfs_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 describe 'cvmfs' do
 
   context 'with defaults for all parameters' do
-    let(:facts) {{ :osfamily => 'RedHat', :uptime_days => 1,
+    let(:facts) {{ :osfamily => 'RedHat', 
+                   :uptime_days => 1,
                    :operatingsystemrelease => '7.1.1503',
+                   :operatingsystemmajrelease => '7',
+                   :architecture => 'x86_64',
                    :kernelrelease => '3.10.0-229.1.2.el7.x86_64' }}
 
     it { should contain_class('cvmfs::install') }
@@ -15,12 +18,58 @@ describe 'cvmfs' do
       let(:facts) {{:osfamily => 'RedHat',
                     :uptime_days => 1,
                     :operatingsystemrelease => '7.1.1503',
+                    :operatingsystemmajrelease => '7',
                     :kernelrelease => '3.10.0-229.1.2.el7.x86_64',
+                    :architecture => 'x86_64',
                     :cvmfsversion => '2.1.20', :cvmfspartsize => '20000'}}
       it { should contain_class('cvmfs::config') }
       it { should contain_class('cvmfs::service') }
       it { should contain_service('autofs') }
 
+      # cvmfs-config repository should be disable by default.
+      #
+      it { should contain_yumrepo('cvmfs').with(
+           'enabled' => '1',
+           'baseurl' => 'http://cern.ch/cvmrepo/yum/cvmfs/EL/7/x86_64' ,
+           'gpgcheck' => '1',
+           'gpgkey'   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM')
+      }
+      it { should contain_yumrepo('cvmfs-testing').with(
+           'enabled' => '0',
+           'baseurl' => 'http://cern.ch/cvmrepo/yum/cvmfs-testing/EL/7/x86_64' ,
+           'gpgcheck' => '1',
+           'gpgkey'   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM')
+      }
+      it { should contain_yumrepo('cvmfs-config').with(
+           'enabled' => '0',
+           'baseurl' => 'http://cern.ch/cvmrepo/yum/cvmfs-config/EL/7/x86_64' ,
+           'gpgcheck' => '1',
+           'gpgkey'   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM')
+      }
+
+      context 'with cvmfs_yum_config_enabled set to 1' do
+        let(:params) {{:cvmfs_yum_config_enabled => '1'}}
+        it { should contain_yumrepo('cvmfs-config').with_enabled('1') }
+      end
+
+      context 'with cvmfs_yum_config set to http://example.org/yum' do
+        let(:params) {{:cvmfs_yum_config => 'http://example.org/yum'}}
+        it { should contain_yumrepo('cvmfs-config').with_baseurl('http://example.org/yum') }
+      end
+
+      context 'with cvmfs_yum_gpgcheck set to 0' do
+        let(:params) {{:cvmfs_yum_gpgcheck => '0'}}
+        it { should contain_yumrepo('cvmfs').with_gpgcheck('0') }
+        it { should contain_yumrepo('cvmfs-testing').with_gpgcheck('0') }
+        it { should contain_yumrepo('cvmfs-config').with_gpgcheck('0') }
+      end
+
+      context 'with cvmfs_yum_gpgkey set to http://example.org/key.gpg' do
+        let(:params) {{:cvmfs_yum_gpgkey => 'http://example.org/key.gpg'}}
+        it { should contain_yumrepo('cvmfs').with_gpgkey('http://example.org/key.gpg') }
+        it { should contain_yumrepo('cvmfs-testing').with_gpgkey('http://example.org/key.gpg') }
+        it { should contain_yumrepo('cvmfs-config').with_gpgkey('http://example.org/key.gpg') }
+      end
 
       context 'with manage_autofs_service true' do
         let(:params) {{:manage_autofs_service => true}}
