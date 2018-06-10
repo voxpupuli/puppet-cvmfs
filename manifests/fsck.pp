a4e117deb579393ed0dcdacb752e900b5567c3a2
Oliver Freyermuth <freyermuth@physik.uni-bonn.de>
2018-06-10 03:16:41 +0200

    fsck: Fix path for awk.
    It's /usr/bin/awk, not /bin/awk. The latter works
    on accident on RHEL-based distros since /bin is a symlink
    to /usr/bin.

--- manifests/fsck.pp
+++ manifests/fsck.pp
@@ -37,12 +37,12 @@ class cvmfs::fsck (
     hour    => fqdn_rand(24,'cvmfs'),
     minute  => fqdn_rand(60,'cvmfs'),
     weekday => fqdn_rand(7,'cvmfs'),
-    command => '/usr/local/sbin/cvmfs_fsck_cron.sh -i 86400  2>&1 | /bin/awk \'{ print strftime("\%Y-\%m-\%d \%H:\%M:\%S"), $0; }\'  >> /var/log/cvmfs_fsck.log',
+    command => '/usr/local/sbin/cvmfs_fsck_cron.sh -i 86400  2>&1 | /usr/bin/awk \'{ print strftime("\%Y-\%m-\%d \%H:\%M:\%S"), $0; }\'  >> /var/log/cvmfs_fsck.log',
     require => [File['/usr/local/sbin/cvmfs_fsck_cron.sh'],Package[$tmpcleaning_pkg]],
   }
   if $onreboot {
     cron{'cvmfs_fsck_on_reboot':
-      command => '/usr/local/sbin/cvmfs_fsck_cron.sh -i 0 2>&1 | /bin/awk \'{ print strftime("\%Y-\%m-\%d \%H:\%M:\%S"), $0; }\'  >> /var/log/cvmfs_fsck.log',
+      command => '/usr/local/sbin/cvmfs_fsck_cron.sh -i 0 2>&1 | /usr/bin/awk \'{ print strftime("\%Y-\%m-\%d \%H:\%M:\%S"), $0; }\'  >> /var/log/cvmfs_fsck.log',
       special => 'reboot',
       require => File['/usr/local/sbin/cvmfs_fsck_cron.sh'],
     }
