d8b787fc328b457384be9c7017a1e888e78caacb
Oliver Freyermuth <o.freyermuth@googlemail.com>
2019-03-27 14:37:02 +0100

    Make $cvmfs_yum_includepkgs an array, join with spaces for yumrepo.

--- manifests/yum.pp
+++ manifests/yum.pp
@@ -24,13 +24,19 @@ class cvmfs::yum (
   Integer $cvmfs_yum_priority = $cvmfs::cvmfs_yum_priority,
 )  inherits cvmfs {
 
+  if $cvmfs_yum_includepkgs =~ Array[String] {
+    $yum_includepkgs = join($cvmfs_yum_includepkgs, ' ')
+  } else {
+    $yum_includepkgs = $cvmfs_yum_includepkgs
+  }
+
   yumrepo{'cvmfs':
     descr       => "CVMFS yum repository for el${::operatingsystemmajrelease}",
     baseurl     => $cvmfs_yum,
     gpgcheck    => $cvmfs_yum_gpgcheck,
     gpgkey      => $cvmfs_yum_gpgkey,
     enabled     => 1,
-    includepkgs => $cvmfs_yum_includepkgs,
+    includepkgs => $yum_includepkgs,
     priority    => $cvmfs_yum_priority,
     require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM'],
     proxy       => $cvmfs_yum_proxy,
@@ -41,7 +47,7 @@ class cvmfs::yum (
     gpgcheck    => $cvmfs_yum_gpgcheck,
     gpgkey      => $cvmfs_yum_gpgkey,
     enabled     => $cvmfs_yum_testing_enabled,
-    includepkgs => $cvmfs_yum_includepkgs,
+    includepkgs => $yum_includepkgs,
     priority    => $cvmfs_yum_priority,
     require     => File['/etc/pki/rpm-gpg/RPM-GPG-KEY-CernVM'],
     proxy       => $cvmfs_yum_proxy,
