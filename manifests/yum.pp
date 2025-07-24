5412d249f56793eac6100ca737b357a3add32611
Valentin Volkl <valentin.volkl@cern.ch>
2025-07-24 14:36:36 +0200

    add cvmfs-future repository

--- manifests/yum.pp
+++ manifests/yum.pp
@@ -3,10 +3,12 @@
 #
 class cvmfs::yum (
   Stdlib::Httpurl $repo_base                                            = $cvmfs::repo_base,
+  Stdlib::Httpurl $repo_base_alt                                        = $cvmfs::repo_base_alt,
   Stdlib::Httpurl $repo_gpgkey                                          = $cvmfs::repo_gpgkey,
   Integer $repo_priority                                                = $cvmfs::repo_priority,
   Boolean $repo_config_enabled                                          = $cvmfs::repo_config_enabled,
   Boolean $repo_testing_enabled                                         = $cvmfs::repo_testing_enabled,
+  Boolean $repo_future_enabled                                          = $cvmfs::repo_future_enabled,
   Optional[Stdlib::Httpurl] $repo_proxy                                 = $cvmfs::repo_proxy,
   Boolean $repo_gpgcheck                                                = $cvmfs::repo_gpgcheck,
   Optional[Variant[Enum['absent'], Array[String[1]]]] $repo_includepkgs = $cvmfs::repo_includepkgs,
@@ -38,9 +40,16 @@ class cvmfs::yum (
   }
 
   yumrepo { 'cvmfs-testing':
-    descr   => "CVMFS yum testing repository for ${_dir}${facts['os']['release']['major']}",
+    descr   => "CVMFS-testing yum repository for ${_dir}${facts['os']['release']['major']}. Same binaries as production repo, released earlier. Very stable.",
     baseurl => "${repo_base}/cvmfs-testing/${_dir}/${facts['os']['release']['major']}/${facts['os']['architecture']}",
     enabled => $repo_testing_enabled,
+  }
+
+  yumrepo { 'cvmfs-future':
+    descr   => "CVMFS-future yum repository for ${_dir}${facts['os']['release']['major']}. Tagged pre-releases. Stable.",
+    # note the use of repo_base_alt - this is in principle a mirror of repo_base, but prereleases are published there first
+    baseurl => "${repo_base_alt}/cvmfs-future/${_dir}/${facts['os']['release']['major']}/${facts['os']['architecture']}",
+    enabled => $repo_future_enabled,
   }
 
   yumrepo { 'cvmfs-config':
