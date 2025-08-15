40be8730decb618f5afe0e1b72b838c8125a43f2
Christoph Maser <christoph.maser+github@gmail.com>
2025-08-15 13:58:25 +0200

    refactor: change from mocha to rspec-mocks
    Ref: https://github.com/voxpupuli/plumbing/issues/78

--- spec/unit/facter/cvmfspartsize_spec.rb
+++ spec/unit/facter/cvmfspartsize_spec.rb
@@ -5,15 +5,15 @@ require 'spec_helper'
 describe 'cvmfspartsize' do
   before do
     Facter.clear
-    Facter::Util::Resolution.stubs(:which).with('df').returns('/usr/bin/df')
-    File.stubs(:exist?).with('/etc/cvmfs/cvmfsfacts.yaml').returns(true)
-    File.stubs(:open).with('/etc/cvmfs/cvmfsfacts.yaml').returns("---\ncvmfs_cache_base: /foo/bar\n")
-    Facter::Core::Execution.stubs(:execute).with('/usr/bin/df -m -P /foo/bar').returns(cvmfs_df_result)
+    allow(Facter::Util::Resolution).to receive(:which).with('df').and_return('/usr/bin/df')
+    allow(File).to receive(:exist?).with('/etc/cvmfs/cvmfsfacts.yaml').and_return(true)
+    allow(File).to receive(:open).with('/etc/cvmfs/cvmfsfacts.yaml').and_return("---\ncvmfs_cache_base: /foo/bar\n")
+    allow(Facter::Core::Execution).to receive(:execute).with('/usr/bin/df -m -P /foo/bar').and_return(cvmfs_df_result)
   end
 
   context 'working case' do
     before do
-      File.stubs(:exist?).with('/foo/bar').returns(true)
+      allow(File).to receive(:exist?).with('/foo/bar').and_return(true)
     end
 
     let(:cvmfs_df_result) { "/dev/nvme0n1p5            976   251       659      28% /foo/bar\n" }
@@ -27,20 +27,20 @@ describe 'cvmfspartsize' do
     let(:cvmfs_df_result) { :failed }
 
     it 'does not return a fact' do
-      File.stubs(:exist?).with('/etc/cvmfs/cvmfsfacts.yaml').returns(false)
+      allow(File).to receive(:exist?).with('/etc/cvmfs/cvmfsfacts.yaml').and_return(false)
       expect(Facter.fact('cvmfspartsize').value).to be_nil
     end
   end
 
   context 'df fails' do
     before do
-      File.stubs(:exist?).with('/foo/bar').returns(false)
+      allow(File).to receive(:exist?).with('/foo/bar').and_return(false)
     end
 
     let(:cvmfs_df_result) { :failed }
 
     it 'does not return a fact' do
-      Facter::Core::Execution.stubs(:execute).with('/usr/bin/df -m -P /foo/bar').returns(:failed)
+      allow(Facter::Core::Execution).to receive(:execute).with('/usr/bin/df -m -P /foo/bar').and_return(:failed)
       expect(Facter.fact('cvmfspartsize').value).to be_nil
     end
   end
